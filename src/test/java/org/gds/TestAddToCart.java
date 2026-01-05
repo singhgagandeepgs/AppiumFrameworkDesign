f765ff491b716100557115048b6c6922400c21d3
Gagandeep Singh <singh.gagandeepgs@gmail.com>
2026-01-04 20:34:18 -0500

    Test Case: Validate Add To Cart Functionality

---
+++ src/test/java/org/gds/TestAddToCart.java
@@ -0,0 +1,28 @@
+
+package org.gds;
+
+import org.gds.pages.ProductCatalogPage;
+import org.testng.annotations.Test;
+
+public class TestAddToCart extends BaseTest {
+	
+	ProductCatalogPage catalogPage;
+
+	@Test
+	public void testAddToCart() throws InterruptedException {
+
+		formPage.setNameField("Gagandeep Singh");
+		formPage.setGender("Male");
+		formPage.selectCountry("India");
+		catalogPage = formPage.submitForm();
+		
+		catalogPage.addToCartByIndex(0);
+		catalogPage.addToCartByIndex(0);
+		catalogPage.goToCart();
+	}
+}
+
+/*
+ * ### V.Imp Learning: To prevent StaleElementException, use locator instead of
+ * webelement in the ExpectedConditions methods for Explicit Wait
+ */
